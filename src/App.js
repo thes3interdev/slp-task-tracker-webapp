b6bae1aaa660cb60fa67498fc99ecf9b1fdff90d
Michael Owuor <michael.owuor@gmail.com>
2022-03-05 16:13:54 +0300

    feat: global state.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
-function App() {
+const App = () => {
+	const [tasks, setTasks] = useState([
+		{ id: 1, text: 'Doctors appointmant', day: 'Feb 5th as 2:30pm', reminder: true },
+		{ id: 2, text: 'PTA meeting', day: 'Feb 6th as 5:30pm', reminder: true },
+		{ id: 3, text: 'Shopping', day: 'Feb 7th as 10:30am', reminder: false },
+	]);
+
 	return (
 		<div className="container">
 			<Header title="Task Manager" />
-			<Tasks />
+			<Tasks tasks={tasks} />
 		</div>
 	);
-}
+};
 
 export default App;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,4 @@
-const tasks = [
-	{ id: 1, text: 'Doctors appointmant', day: 'Feb 5th as 2:30pm', reminder: true },
-	{ id: 2, text: 'PTA meeting', day: 'Feb 6th as 5:30pm', reminder: true },
-	{ id: 3, text: 'Shopping', day: 'Feb 7th as 10:30am', reminder: false },
-];
-
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
 	return (
 		<>
 			{tasks.map((task) => (
