8730d7728c54719dbf6dfbf273b884d314b9234a
Michael Owuor <michael.owuor@gmail.com>
2022-03-05 16:34:39 +0300

    feat: create single task component.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Tasks from './components/Tasks';
 
 const App = () => {
 	const [tasks, setTasks] = useState([
-		{ id: 1, text: 'Doctors appointmant', day: 'Feb 5th as 2:30pm', reminder: true },
-		{ id: 2, text: 'PTA meeting', day: 'Feb 6th as 5:30pm', reminder: true },
-		{ id: 3, text: 'Shopping', day: 'Feb 7th as 10:30am', reminder: false },
+		{ id: 1, text: 'Doctors appointmant', day: 'Feb 5th at 2:30pm', reminder: true },
+		{ id: 2, text: 'PTA meeting', day: 'Feb 6th at 5:30pm', reminder: true },
+		{ id: 3, text: 'Shopping', day: 'Feb 7th at 10:30am', reminder: false },
 	]);
 
 	return (
